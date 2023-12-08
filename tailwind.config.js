6f0cbc92c51c9e222ce657cbd5b1abc8cd235431
Khaled Almansour <Khaled@mugendai.io>
2023-12-08 13:35:28 +0300

    First Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primaryLightGreen: '#ECF4D6',
+        primaryGreen: '#9AD0C2',
+        secondaryGreen: '#2D9596',
+        primaryBlue: '#265073',
+        primaryGray: '#D9D9D9',
+
+      }
     },
   },
   plugins: [],
